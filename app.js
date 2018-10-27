a6c34c6d38fec0fc4b9ff8c5e9023ad26d52e35a
Roger Diller <roger@flexrentalsolutions.com>
2018-10-27 13:42:54 -0400

    lotsa fun JS features

--- app.js
+++ app.js
@@ -9,6 +9,45 @@ Ext.application({
     ],
 
     launch: function () {
-        Ext.Msg.alert('Hello Ext JS', 'Hello! Welcome to Ext JS.');
+        //ES6
+
+        //let keyword
+        let materials = [
+            'Hydrogen',
+            'Helium',
+            'Lithium'
+        ];
+
+        //arrow functions
+        console.log(materials.map(material => material.length));
+
+        //let re-assign
+        materials = 'blah';
+        console.log(materials);
+
+        //string interpolation
+        const hello = 'Hello (from ES6+)';
+        Ext.Msg.alert(`${hello} Ext JS`, 'Hello! Welcome to Ext JS.');
+
+        //default params
+        this.add();
+        this.add(1);
+        this.add(2,3);
+
+        //ES7
+
+        //Exponentiation
+        // let mathValue = 2 ** 8;
+        // console.log(mathValue); //256
+
+        //ES8
+
+        //string padding methods
+        console.log('es8-pad'.padStart(10))
+
+    },
+
+    add: function (x=5, y=10) {
+        console.log(x + y);
     }
 });
